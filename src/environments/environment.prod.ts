ae08b03bf5bec726d6bac2d6d770a5477a94c8a5
Finamufidz <finamufidz@gmail.com>
2024-07-27 17:59:59 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  apiAnak:
+    'https://955890c9-b8e9-4c52-8118-03b257f7bfa4-00-2mvlipatfnihr.kirk.replit.dev/',
+  apiOrtu:
+    'https://42052531-e7b7-4623-b2a0-c9c2a1dbbedf-00-3rgszf12nshpt.kirk.replit.dev/',
+};  
